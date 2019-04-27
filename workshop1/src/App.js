9d4534d727d1071e3a93f32f3959bf7ab1dd4420
ANIMESH KUMAR <animeshk55@gmail.com>
2019-04-27 14:58:14 +0530

    Basic todo app

--- workshop1/src/App.js
+++ workshop1/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { score: 0 };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+    // console.log("increment");
+  };
+
+  decrement = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+    // console.log("decrement");
+  };
+  render() {
+    return (
+      <div>
+        <h2>Counter Button</h2>
+        <div className="row">
+          <h3
+            onClick={this.increment}
+            style={{ fontSize: 50, margin: 20, padding: 20 }}
+          >
+            -
+          </h3>
+          <h3>{this.state.score}</h3>
+          <h3
+            onClick={this.decrement}
+            style={{ fontSize: 50, margin: 20, padding: 20 }}
+          >
+            +
+          </h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
